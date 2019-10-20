1809077dde4abfe1deafe7a7afbb4dd6d96c1f84
Melike KILIC <melike180400@gmail.com>
2019-10-20 19:15:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def monopoly_with_third_tier
 				:three_pieces_owned=> 100,
 				:four_pieces_owned => 200
 				}
+			:names =>4
 			}
 }
   # When you start writing the implementation for this method, copy the Hash

8a12c600e4ed14341a6874975eb411285e27abb9
Melike KILIC <melike180400@gmail.com>
2019-10-20 18:36:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,6 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{:key=> "Make sure you have at least one key defined in the monopoly hash"}
+	return :key
 end
 
 def monopoly_with_second_tier

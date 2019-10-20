99d9672383fbb68d1f82595996fd36bf8a348948
Melike KILIC <melike180400@gmail.com>
2019-10-20 19:14:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def monopoly_with_third_tier
 				:three_pieces_owned=> 100,
 				:four_pieces_owned => 200
 				}
+			}
 }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
